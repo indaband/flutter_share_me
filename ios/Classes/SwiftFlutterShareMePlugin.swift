dc504bd779a328fdfe7511561e770af948dc6b0b
Andrews Medina <andrewsmedina@gmail.com>
2025-12-05 13:36:30 -0300

    fix dialog

--- ios/Classes/SwiftFlutterShareMePlugin.swift
+++ ios/Classes/SwiftFlutterShareMePlugin.swift
@@ -195,7 +195,7 @@ public class SwiftFlutterShareMePlugin: NSObject, FlutterPlugin, SharingDelegate
         shareContent.contentURL = URL.init(string: message["url"] as! String)!
         shareContent.quote = message["msg"] as? String
         
-        let shareDialog = ShareDialog(viewController: viewController!, content: shareContent, delegate: self)
+        let shareDialog = ShareDialog.dialog(viewController: viewController!, content: shareContent, delegate: self)
         shareDialog.mode = .automatic
         shareDialog.show()
         result("Sucess")
